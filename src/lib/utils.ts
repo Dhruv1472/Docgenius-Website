1c47fdafd409ab9f0cf8afa6392347b84cba5211
dhruv <dhruv.k@mvclouds.com>
2026-02-12 17:25:41 +0530

    add package link

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -3,7 +3,7 @@ import { twMerge } from "tailwind-merge";
 
 export const CTA_LINKS = {
   bookDemo: "https://mvclouds.com/contact-us",
-  freeTrial: "https://mvclouds.com/",
+  freeTrial: "https://login.salesforce.com/packaging/installPackage.apexp?p0=04tgK0000009iJZ",
   privacyPolicy: "https://mvclouds.com/products/docgenius/privacy-policy",
   termsOfUse: "https://mvclouds.com/products/docgenius/privacy-policy",
 };
