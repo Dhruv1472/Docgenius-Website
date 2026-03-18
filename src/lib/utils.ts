0f7fa978f6e658410883c188aef5485d4f8de42a
KevinSuvagiya01 <kevin.s@mvclouds.com>
2026-03-18 11:11:44 +0530

    fix: Update freeTrial link to correct Salesforce package installation URL

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -3,7 +3,7 @@ import { twMerge } from "tailwind-merge";
 
 export const CTA_LINKS = {
   bookDemo: "https://mvclouds.com/contact-us",
-  freeTrial: "https://login.salesforce.com/packaging/installPackage.apexp?p0=04tgK000000A4Tl",
+  freeTrial: "https://login.salesforce.com/packaging/installPackage.apexp?p0=04tgK000000AhTV",
   privacyPolicy: "https://mvclouds.com/privacypolicy",
   termsOfUse: "https://mvclouds.com/privacypolicy",
 };
